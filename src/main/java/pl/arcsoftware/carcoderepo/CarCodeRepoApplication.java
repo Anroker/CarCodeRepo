b66303b2c56593c31a10ccab5ab16e8609399e7f
Anrok <anrok1234@gmail.com>
2022-04-12 15:02:45 +0200

    Initialization

---
+++ src/main/java/pl/arcsoftware/carcoderepo/CarCodeRepoApplication.java
@@ -0,0 +1,13 @@
+package pl.arcsoftware.carcoderepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCodeRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarCodeRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/arcsoftware/carcoderepo/CarCodeRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.arcsoftware.carcoderepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCodeRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
