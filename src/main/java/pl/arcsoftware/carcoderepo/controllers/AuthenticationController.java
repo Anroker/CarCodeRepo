8f918f324e2a1ceeb30a2c418c40f9190090874b
Darnoker <73139334+Darnoker@users.noreply.github.com>
2022-04-21 12:35:39 +0200

    add AuthenticationController

---
+++ src/main/java/pl/arcsoftware/carcoderepo/controllers/AuthenticationController.java
@@ -0,0 +1,48 @@
+package pl.arcsoftware.carcoderepo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pl.arcsoftware.carcoderepo.config.TokenProvider;
+import pl.arcsoftware.carcoderepo.models.AuthToken;
+import pl.arcsoftware.carcoderepo.models.LoginUser;
+import pl.arcsoftware.carcoderepo.models.User;
+import pl.arcsoftware.carcoderepo.services.UserService;
+
+@RestController
+@RequestMapping("/token")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenProvider jwtTokenUtil;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/generate-token", method = RequestMethod.POST)
+    public ResponseEntity register(@RequestBody LoginUser loginUser) throws AuthenticationException {
+
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginUser.getUsername(),
+                        loginUser.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final User user = userService.findOne(loginUser.getUsername());
+        final String token = jwtTokenUtil.generateToken(authentication);
+        return ResponseEntity.ok(new AuthToken(token));
+    }
+
+
+}
